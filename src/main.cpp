112b056b27a51e8a794cb9979af95b7b1bb94d07
hongxin <urusaurusa@qq.com>
2025-07-21 16:24:47 +0800

    update test

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,97 @@
 #include <iostream>
-#include "BaiDuEngine.h"
-#include "URLEncode.h"
-#include <thread>
 #include <chrono>
+#include <thread>
+
+#include "BaiDuEngine.h"
+#include "DeepLEngine.h"
+
+#ifdef _WIN32
+#include <windows.h>
+#endif
 using namespace std;
-int main()
+
+// data set     数据集
+std::string transText1;
+std::string transText2;
+std::string transText3;
+std::string transText4;
+std::vector<std::string> transTextVec;
+
+auto resetFunc = [&]() {
+    transTextVec.clear();
+
+    transText1 = "The beauty of life lies in the journey, not the destination. ";
+    transText2 = "The greatest glory in living lies not in never falling, but in rising every time we fall.";
+    transText3 = u8"苹果";
+    transText4 = u8"小米";
+    transTextVec.push_back(u8"香辣鸡腿堡");
+    transTextVec.push_back(u8"生椰拿铁");
+    transTextVec.push_back(u8"阿玛特拉斯");
+    transTextVec.push_back(u8"我不知道将去何方，但我已在路上。");
+};
+
+auto printFunc = [](const string& src, const string& dst) {
+    cout << "src: " << src << "\tdst: " << dst << endl;
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+};
+auto printVecFunc = [](const std::vector<std::string>& src, const std::vector<std::string>& dst) {
+    cout << "src: "
+         << "\tdst: " << endl;
+    for (int i = 0; i < dst.size(); i++)
+    {
+        cout << src[i] << "\t" << dst[i] << endl;
+    }
+};
+
+void testBaiDuFunc()
 {
-    // 待翻译文本
-    std::string str  = u8"苹果";
-    std::string str2 = u8"雪梨";
-
-    std::vector<std::string> vec;
-    vec.push_back(str);
-    vec.push_back(str2);
-
-    // 编码
-    std::string encode = URLEncode::encode(str);
-    cout << encode << endl;
-
-    // 翻译结果
-    std::string result;
+    resetFunc();
+    cout << "BaiDu Engine:" << endl;
 
     EngineBase* ptr = new BaiDuEngine();
+    ptr->SetFromLanguage("en");
+    ptr->SetToLanguage("zh");
+    printFunc(transText1, ptr->TranslateText(transText1));
+    printFunc(transText2, ptr->TranslateText(transText2));
+
     ptr->SetFromLanguage("zh");
     ptr->SetToLanguage("en");
+    printFunc(transText3, ptr->TranslateText(transText3));
+    printFunc(transText4, ptr->TranslateText(transText4));
 
-    result = ptr->TranslateText(str);
-    cout << result << endl;
+    cout << endl;
+    printVecFunc(transTextVec, ptr->TranslateBatch(transTextVec));
+    cout << endl;
+}
 
-    std::this_thread::sleep_for(std::chrono::seconds(1));
+void testDeepLFunc()
+{
+    resetFunc();
+    cout << "DeepL Engine:" << endl;
 
-    std::vector<std::string> result2;
-    result2 = ptr->TranslateBatch(vec);
-    for (auto& dst : result2)
-    {
-        cout << "dst：" << dst << endl;
-    }
+    EngineBase* ptr = new DeepLEngine();
+    ptr->SetFromLanguage("en");
+    ptr->SetToLanguage("zh");
+    printFunc(transText1, ptr->TranslateText(transText1));
+    printFunc(transText2, ptr->TranslateText(transText2));
+
+    ptr->SetFromLanguage("zh");
+    ptr->SetToLanguage("en");
+    printFunc(transText3, ptr->TranslateText(transText3));
+    printFunc(transText4, ptr->TranslateText(transText4));
+
+    cout << endl;
+    printVecFunc(transTextVec, ptr->TranslateBatch(transTextVec));
+    cout << endl;
+}
+
+int main()
+{
+#ifdef _WIN32
+    SetConsoleOutputCP(CP_UTF8);
+#endif
+
+    testBaiDuFunc();
+
+    testDeepLFunc();
 }
